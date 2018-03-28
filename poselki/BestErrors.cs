4d670b469f9e57dc928c83b36f7bb06700a492eb
Samsonov Eugene <samsonov_eugene@mail.ru>
2018-03-28 19:11:13 +0300

    Повышение производительности, исправление ошибок

--- poselki/BestErrors.cs
+++ poselki/BestErrors.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 
 namespace poselki
 {
@@ -6,7 +8,7 @@ namespace poselki
     {
         public void ExceptionProtector()
         {
-            MessageBox.Show("Произошла ошибка, но умнейшая программа все обработала", "Митинг подавлен", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show("Ой, ой. А что это? Это ошибка, но, к сожалению, я о ней уже знал", "Митинг подавлен", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         public string getError(string NumError)
         {
@@ -25,10 +27,41 @@ namespace poselki
                     NumError = "Вы заполнили одно или несколько полей некорректно, извинитесь перед разработчиком и пообещайте ему что вы больше так не будете";
                     break;
                 default:
-                    NumError = "Произошла неизвестная ошибка. Номер ошибки - " + NumError;
+                    string ErrorToFile = "Произошла неизвестная ошибка. Номер ошибки - " + NumError;
+                    if (LogMessageToFile(ErrorToFile))
+                        NumError = "Произошла неизвестная ошибка. Номер ошибки - " + NumError + ". Отправьте разработчику лог - " + GetTempPath() + "PoselkiLogFile.txt";
+                    else
+                        NumError = "Произошла неизвестная ошибка. Номер ошибки - " + NumError;
                     break;
             }
             return NumError;
+        }
+        private string GetTempPath()
+        {
+            string path = System.Environment.GetEnvironmentVariable("TEMP");
+            if (!path.EndsWith("\\")) path += "\\";
+            return path;
+        }
+
+        private bool LogMessageToFile(string msg)
+        {
+            StreamWriter sw = File.AppendText(
+            GetTempPath() + "PoselkiLogFile.txt");
+            try
+            {
+                string logLine = System.String.Format(
+                    "{0:G}: {1}.", DateTime.Now, msg);
+                sw.WriteLine(logLine);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                sw.Close();
+            }
+            return true;
         }
     }
 }
